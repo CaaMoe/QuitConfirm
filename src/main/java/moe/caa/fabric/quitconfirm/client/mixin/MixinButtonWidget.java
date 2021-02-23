c60994cb6324b781c302fb2c7cdaf20ac29caccf
CaaMoe <Aeva@D@hotmail.com>
2021-02-23 13:38:37 +0800

    修改注入逻辑

--- src/main/java/moe/caa/fabric/quitconfirm/client/mixin/MixinButtonWidget.java
+++ src/main/java/moe/caa/fabric/quitconfirm/client/mixin/MixinButtonWidget.java
@@ -1,18 +1,34 @@
 package moe.caa.fabric.quitconfirm.client.mixin;
 
-import moe.caa.fabric.quitconfirm.client.screen.IButtonWidget;
+import moe.caa.fabric.quitconfirm.client.screen.ToMainPressAction;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.GameMenuScreen;
+import net.minecraft.client.gui.widget.AbstractPressableButtonWidget;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ButtonWidget.class)
-public abstract class MixinButtonWidget implements IButtonWidget {
+public abstract class MixinButtonWidget extends AbstractPressableButtonWidget {
+
+    public MixinButtonWidget(int i, int j, int k, int l, Text text) {
+        super(i, j, k, l, text);
+    }
 
     @Accessor
     protected abstract void setOnPress(ButtonWidget.PressAction onPress);
 
-    @Override
-    public void setTheOnPress(ButtonWidget.PressAction onPress) {
-        setOnPress(onPress);
+    @Inject(method = "<init>(IIIILnet/minecraft/text/Text;Lnet/minecraft/client/gui/widget/ButtonWidget$PressAction;Lnet/minecraft/client/gui/widget/ButtonWidget$TooltipSupplier;)V", at = @At("RETURN"))
+    private void onInit(int x, int y, int width, int height, Text message, ButtonWidget.PressAction onPress, ButtonWidget.TooltipSupplier tooltipSupplier, CallbackInfo ci){
+        if (MinecraftClient.getInstance().currentScreen instanceof GameMenuScreen){
+            if(message instanceof TranslatableText && ((TranslatableText) message).getKey().equals("menu.returnToMenu")){
+                setOnPress(new ToMainPressAction(MinecraftClient.getInstance()));
+            }
+        }
     }
 }
--- src/main/java/moe/caa/fabric/quitconfirm/client/screen/IButtonWidget.java
+++
@@ -1,7 +0,0 @@
-package moe.caa.fabric.quitconfirm.client.screen;
-
-import net.minecraft.client.gui.widget.ButtonWidget;
-
-public interface IButtonWidget {
-    void setTheOnPress(ButtonWidget.PressAction onPress);
-}
